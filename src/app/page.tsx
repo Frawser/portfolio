e1ab3f9ff8fd7e0a300d67e1a47042031b04136e
Frawser <mikis011@hotmail.com>
2025-10-16 19:32:39 +0200

    Button added and styling updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,15 @@ import About from "@/components/About";
 import Stats from "@/components/Stats";
 import Background from "@/components/Background";
 import Projects from "@/components/Projects";
+import DownloadCVButton from "@/components/DownloadCV";
+
 
 export default function Home() {
   return (
-    <div className="bg-gradient-to-r from-cyan-50 via-white to-cyan-50 dark:from-transparent dark:via-cyan-200/10 dark:to-transparent transition-colors duration-500">
-      <Hero />
+    <div>
+      <DownloadCVButton />
       <DarkModeToggle />
+      <Hero />
       <About />
       <Stats />
       <Background />
